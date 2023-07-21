5ad54ebfd363e7e48dcd1ceb166232b4ee835161
Allanpt <jatallantavares@gmail.com>
2023-07-21 07:49:35 -0300

    Service layer, component registration

---
+++ src/main/java/com/webServicesProjects/Web/_Services_Spring_Boot_JPA_Hibernate/services/UserService.java
@@ -0,0 +1,24 @@
+package com.webServicesProjects.Web._Services_Spring_Boot_JPA_Hibernate.services;
+
+import com.webServicesProjects.Web._Services_Spring_Boot_JPA_Hibernate.entities.User;
+import com.webServicesProjects.Web._Services_Spring_Boot_JPA_Hibernate.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<User> findAll(){
+        return userRepository.findAll();
+    }
+    public User findById(Long id){
+       Optional<User> obj = userRepository.findById(id);
+       return obj.get();
+    }
+}
