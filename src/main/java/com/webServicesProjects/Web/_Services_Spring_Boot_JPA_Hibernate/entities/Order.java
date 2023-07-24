db49ca3458792ee4dcd6a69c016b533ef11fe2b2
Allanpt <jatallantavares@gmail.com>
2023-07-24 10:59:32 -0300

    OrderItem, many-to-many association with extra attributes

--- src/main/java/com/webServicesProjects/Web/_Services_Spring_Boot_JPA_Hibernate/entities/Order.java
+++ src/main/java/com/webServicesProjects/Web/_Services_Spring_Boot_JPA_Hibernate/entities/Order.java
@@ -6,7 +6,9 @@ import jakarta.persistence.*;
 
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 @Entity
 @Table(name = "tb_order")
@@ -26,6 +28,9 @@ public class Order implements Serializable {
     @ManyToOne
     @JoinColumn(name = "customer_id")
     private User customer;
+
+    @OneToMany(mappedBy = "id.order")
+    private Set<OrderItem> items = new HashSet<>();
 
     public Order(){
 
@@ -72,6 +77,10 @@ public class Order implements Serializable {
         this.customer = customer;
     }
 
+    public Set<OrderItem> getItems() {
+        return items;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -84,4 +93,6 @@ public class Order implements Serializable {
     public int hashCode() {
         return Objects.hash(id);
     }
+
+
 }
---
+++ src/main/java/com/webServicesProjects/Web/_Services_Spring_Boot_JPA_Hibernate/repositories/OrderItemRepository.java
@@ -0,0 +1,9 @@
+package com.webServicesProjects.Web._Services_Spring_Boot_JPA_Hibernate.repositories;
+
+import com.webServicesProjects.Web._Services_Spring_Boot_JPA_Hibernate.entities.Category;
+import com.webServicesProjects.Web._Services_Spring_Boot_JPA_Hibernate.entities.OrderItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
+
+}
